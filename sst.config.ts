7e0bb2ee63c3ef8f71db8c226af5196d48cddb2d
andy-h1 <a.huynh2612@gmail.com>
2026-01-17 16:27:50 +0000

    build: setting up repo

--- sst.config.ts
+++ sst.config.ts
@@ -1,15 +1,31 @@
 /// <reference path="./.sst/platform/config.d.ts" />
 
 export default $config({
-  app(input) {
-    return {
-      name: "clinical-letters",
-      removal: input?.stage === "production" ? "retain" : "remove",
-      protect: ["production"].includes(input?.stage),
-      home: "aws",
-    };
-  },
-  async run() {
-    new sst.aws.React("MyWeb");
-  },
+	app(input) {
+		return {
+			name: "clinical-letters",
+			removal: input?.stage === "production" ? "retain" : "remove",
+			protect: ["production"].includes(input?.stage),
+			home: "aws",
+			providers: {
+				aws: {
+					profile:
+						input.stage === "production"
+							? "clinical-letters-production"
+							: "clinical-letters-dev",
+				},
+			},
+		};
+	},
+	async run() {
+		const bucket = new sst.aws.Bucket("LettersBucket");
+
+		const web = new sst.aws.React("Web", {
+			link: [bucket],
+		});
+
+		return {
+			web: web.url,
+		};
+	},
 });
